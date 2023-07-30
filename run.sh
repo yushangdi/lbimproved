dd3590ed2664967729fe55d4fa25ed1ff4280c2f
sy <shangdiy@csail.mit.edu>
2023-07-30 12:26:40 -0400

    add running commands

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# time ./example knnGraph/datasets/Crop.csv 0 10
+# time ./example knnGraph/datasets/ElectricDevices.csv 0.14 10
+time ./example knnGraph/datasets/StarLightCurves.csv 0.16 10
+time ./example knnGraph/datasets/Wafer.csv 0.01 10
+time ./example knnGraph/datasets/TwoPatterns.csv 0.04 10
+time ./example knnGraph/datasets/ECG5000.csv 0.01 10
+time ./example knnGraph/datasets/FordA.csv 0.01 10
+time ./example knnGraph/datasets/UWaveGestureLibraryAll.csv 0.04 10
+time ./example knnGraph/datasets/UWaveGestureLibraryX.csv 0.04 10
+time ./example knnGraph/datasets/UWaveGestureLibraryY.csv 0.04 10
